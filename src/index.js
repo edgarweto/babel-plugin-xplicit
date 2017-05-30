600c3a057d0c9a6ab07518c8fb02f4368c126aa4
Edgar <komono.32@gmail.com>
2017-05-30 09:08:27 +0200

    added plugin body, more tests

--- src/index.js
+++ src/index.js
@@ -1,20 +1,142 @@
 const DEFAULT_OPTIONS = {
   verbs: {
     assert: 'console.assert'
+  },
+  globalScopeName: 'global'
+};
+
+/**
+ * @desc This trace plugin keeps the trace number, which is increased for each
+ *  found trace label. We use this object to save its internal, static state.
+ */
+const pluginState = {
+  count: function () {
+    if (!this._count) {
+      this._count = 1;
+    }
+    return this._count++;
+  },
+  resetState: function () {
+    this._count = 1;
   }
 };
 
+const _toExpression = function (node) {
+  return node.expression || node;
+};
+
+
+
+
 const plugin = function ({types: t}) {
+
+  const generateCode = function generateCode(path, node, scope, opts) {    
+    const nodeType = node.type,
+      visitor = assertBodyVisitor[nodeType];
+    if (visitor) {
+      return visitor(path, node, scope, opts);
+    } else {
+      throw path.buildCodeFrameError(`Not implemented ${nodeType} inside trace`);
+    }
+  };
+
+
+  const assertBodyVisitor = {
+
+    ExpressionStatement: function (path, node, scope, opts) {
+      if (opts.genAssert) {
+        const scopeName = scope.scopeName,
+          msg = "[" + pluginState.count() + "]" + (scopeName ? " " + scopeName : "") + ": ";
+        
+        const withMessage = false;
+        let params = [];
+
+        if (withMessage) {
+          params = [node.expression, t.stringLiteral(msg)];
+        } else {
+          params = [node.expression];
+        }
+        return t.expressionStatement(t.callExpression(t.identifier(opts.asserterFn), params));
+      } else {
+        return node.expression;
+      }
+    },
+
+    BlockStatement: function (path, node, scope, opts) {
+      const scopeName = scope.scopeName;
+      
+      const statements = [];
+      node.body.forEach(function (stmt) {
+
+        if (opts.genAssert) {
+          let msg = "[" + pluginState.count() + "]" + (scopeName ? " " + scopeName : "") + ": ";
+
+          statements.push(t.expressionStatement(t.callExpression(t.identifier(opts.asserterFn), [
+            t.stringLiteral(msg),
+            _toExpression(generateCode(path, stmt, scope, {
+              genAssert: false,
+              asserterFn: opts.asserterFn
+            }))
+          ])));
+        } else {
+          statements.push(_toExpression(generateCode(path, stmt, scope, opts)));
+        }
+      });
+      return statements;
+    },
+
+    EmptyStatement: function (path, node, scope, opts) {
+      return [];
+    }
+  };
+
+
   return {
     visitor: {
-      LabelStatement(path, state) {
-        const options = state.opts;
-        const labelName = path.node.label.name,
-          asserter = options[labelName] || DEFAULT_OPTIONS[labelName];
+      LabeledStatement(path, state) {
+        const options = state.opts,
+          labelName = path.node.label.name,
+          asserter = options[labelName] || DEFAULT_OPTIONS.verbs[labelName];
 
         if (asserter) {
           if (options.strip) {
             path.remove();
+          } else {
+
+            // 1. Process message: function's name, line and column.
+            const parentFn = path.getFunctionParent(),
+              globalScopeName = state.opts.globalScopeName || DEFAULT_OPTIONS.globalScopeName,
+              fnName = parentFn && parentFn.node.id && parentFn.node.id.name || false;
+
+            let scopeName = parentFn && parentFn.node.id && parentFn.node.id.name || globalScopeName;
+
+            // If  there is no function name, try to check if we are inside class member functions
+            if (!fnName) {
+              const classMethod = path.findParent((classMethod) => classMethod.isClassMethod());
+              scopeName = classMethod ? classMethod.node.key.name : scopeName;
+
+              const classDef = path.findParent((classMethod) => classMethod.isClassDeclaration());
+              if (classDef) {
+                const classname = classDef.node.id.name;
+                scopeName = classname + '::' + scopeName;
+              }
+
+            }
+
+            // 2. Process the body
+            const assertCode = generateCode(path, path.node.body, {
+              scopeName: scopeName
+            }, {
+              genAssert: true,
+              asserterFn: asserter
+            });
+
+
+            if (Array.isArray(assertCode)) {
+              path.replaceWithMultiple(assertCode);
+            } else {
+              path.replaceWith(assertCode);
+            }
           }
         }
       }
@@ -22,5 +144,8 @@ const plugin = function ({types: t}) {
   };
 };
 
+plugin.resetState = function () {
+  pluginState.resetState();
+};
 
 export default plugin;
--- test/index.js
+++ test/index.js
@@ -1,3 +1,9 @@
+const DEFINITIONS = {
+  envProd: 'prod',
+  envDev: 'dev'
+};
+
+
 const assert = require('chai').assert,
   transformFileSync = require('babel-core').transformFileSync,
   fs = require('fs'),
@@ -10,3 +16,57 @@ describe('Plugin definition', function () {
     assert.typeOf(plugin, 'function');
   });
 });
+
+
+/**
+ * @desc List of fragments to be tested.
+ */
+const fragments = [{
+    file: 'fgGlobals01',
+    env: DEFINITIONS.envProd
+  }, {
+    file: 'fgGlobals02',
+    env: DEFINITIONS.envDev
+  }];
+
+
+describe('Fragments', function () {
+  fragments.forEach(function (fragment) {
+      if (fragment.fail) {
+      } else {
+        it(`'test fragment '${fragment.file}'`, function (done) {
+          const transpiled = transpileFragment(plugin, fragment),
+            fgExpected = `fragments/${fragment.file}Expected.js`,
+            expected = fs.readFileSync(path.join(__dirname, fgExpected)).toString();
+          assert.equal(uniformifyJs(transpiled.code), uniformifyJs(expected));
+          done();
+        });
+      }
+  });
+});
+
+
+function transpileFragment(plugin, fragment) {
+  plugin.resetState();
+
+  const fragmentFile = `test/fragments/${fragment.file}.js`,
+    pluginOptions = {
+      strip: fragment.env === DEFINITIONS.envProd ? true : false
+    };
+
+  Object.assign(pluginOptions, fragment.options);
+
+  return transformFileSync(fragmentFile, {
+    plugins: [[plugin, pluginOptions]],
+    babelrc: false
+  });
+}
+
+/**
+ * @desc Transform javascript text code in order to ease comparisons.
+ * @param {string} code Javascript text from fixtures or transpilations.
+ * @return {string} non-valid js code, suitable for comparisons.
+ */
+function uniformifyJs(code) {
+  return code.replace(/(\r\n|\n|\r|\s+)/gm, "");
+}
